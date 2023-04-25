95f42171c8c257d0e0f9682474e2985dd84d3342
britotiago03 <69848652+britotiago03@users.noreply.github.com>
2023-04-26 00:10:47 +0200

    v1.7.1 Fixed version in Server and pom.xml

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -25,7 +25,7 @@ import org.example.logic.UsernameCollection;
  */
 public class Server {
   private static final int PORT = 8080;
-  public static final String VERSION = "1.7.0";
+  public static final String VERSION = "1.7.1";
   private final Map<Lobby, GameSettings> lobbyGameSettingsMap;
   private final Map<String, Lobby> nameLobbyMap;
   private final Map<String, GameCollection> stringGameMap;
